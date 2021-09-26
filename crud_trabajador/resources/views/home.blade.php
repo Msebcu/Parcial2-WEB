4573f5e55c725f34c2d934ac6e700679337e8e04
Msebcu <msebc@miumg.edu.gt>
2021-09-25 23:02:43 -0600

    CRUD: Agregar página inicio, página home y opciones de registro

--- crud_trabajador/resources/views/home.blade.php
+++ crud_trabajador/resources/views/home.blade.php
@@ -4,28 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
             <div class="mb-3">
-                <label for="Nombre" class="form-label">Email address</label>
-                <input type="Nombre" class="form-control" id="exampleFormControlInput1" placeholder="Nombres apellidos">
+                <label for="Nombre" class="form-label">Nombres</label>
+                <input type="Nombre" class="form-control" id="exampleFormControlInput1" placeholder="Juan Carlos">
               </div>
               <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <label for="Nombre" class="form-label">Apellidos</label>
+                <input type="Nombre" class="form-control" id="exampleFormControlInput1" placeholder="Plata">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Dirección</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
               </div>
-              
+
+              <button type="button" class="btn btn-primary">
+                  <li class="fas fa-user-plus"></li>
+                  Registrar
+              </button>
+
         </div>
     </div>
 </div>
--- crud_trabajador/resources/views/layouts/app.blade.php
+++ crud_trabajador/resources/views/layouts/app.blade.php
@@ -19,6 +19,9 @@
     <!--Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/6434bdacf9.js" crossorigin="anonymous"></script>
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
